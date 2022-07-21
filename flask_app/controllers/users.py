b9586a915fcee9fcbf1ef1b7c36acf6fc51d63c0
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2022-07-21 14:11:15 +0800

    constructed mysqlconnectiomn.py, controller, model

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,8 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    users = User.get_all()
+    return render_template('index.html',users=users)
